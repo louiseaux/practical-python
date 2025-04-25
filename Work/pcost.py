e6ca877cf738278160af5594330dee5b8ac10f3b
Ashlyn <ashlyn.barry1@gmail.com>
2025-04-25 16:10:19 +1000

    modified pcost.py for exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,8 @@
 #
 # Exercise 1.27
 
+import csv
+
 def portfolio_cost(filename):
     '''
     Computes the total cost (shares*price) of a portfolio file
@@ -9,10 +11,10 @@ def portfolio_cost(filename):
     total_cost = 0.0
 
     with open(filename, 'rt') as f:
-        headers = next(f)
-        for line in f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
             try:
-                row = line.split(',')
                 shares = int(row[1])
                 price = float(row[2])
                 total_cost += shares * price
