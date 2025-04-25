55076c3979288ee4f651f1fee0db11e1d49c7013
Ashlyn <ashlyn.barry1@gmail.com>
2025-04-25 13:29:57 +1000

    modified mortgage.py for exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,15 +8,16 @@ payment = 2684.11
 total_paid = 0.0
 month = 0
 
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 extra_payment = 1000
-extra_payment_end_month = 12
 
 while principal > 0:
     month += 1
     principal = principal * (1+rate/12) - payment
     total_paid += payment
 
-    if month <= extra_payment_end_month:
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
         principal -= extra_payment
         total_paid += extra_payment
 
