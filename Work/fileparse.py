a30aa689429f35892cdb3cb4a3935c880b2801f3
Ashlyn <ashlyn.barry1@gmail.com>
2025-04-29 13:38:45 +1000

    completed picking a different column delimiter (ex 3.7)

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,12 +4,12 @@
 
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     '''
-    Parse a CSV file into a list of records
+    Parse a CSV file into a list of records with type conversion.
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
         # Read the file headers (if any)
         headers = next(rows) if has_headers else []
