ffcb7e15967368f950c492a2ab454a67397e51a8
Ashlyn <ashlyn.barry1@gmail.com>
2025-05-01 16:11:12 +1000

    made pipeline components (ex 6.10)

--- Work/follow.py
+++ Work/follow.py
@@ -9,7 +9,7 @@ def follow(filename):
     '''
     Generator that produces a sequence of lines being written at the end of a file.
     '''
-    f = open(filename)
+    f = open(filename, 'r')
     f.seek(0, os.SEEK_END)    # Move file pointer 0 bytes from end of file
 
     while True:
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     portfolio = report.read_portfolio('Data/portfolio.csv')
 
     for line in follow('Data/stocklog.csv'):
-        fields = line.split(',')
-        name = fields[0].strip('"')
-        price = float(fields[1])
-        change = float(fields[4])
+        row = line.split(',')
+        name = row[0].strip('"')
+        price = float(row[1])
+        change = float(row[4])
         if name in portfolio:
             print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
